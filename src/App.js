bf85651a0e5e8106ce9b5086a3d6df5a80344c3e
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2021-10-17 17:35:43 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+import { apiKey } from './apiKey';
 
 function App() {
+
+  const [currentUsersIP, setCurrentUsersIP] = useState(null);
+
+  useEffect(() => {
+
+  })
+
+  // console.log(apiKey)
   return (
     <div className="App">
     <p>We in boys</p>
